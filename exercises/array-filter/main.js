ac972934167b77b900a5171676aa6a119a0e38c7
Marlon <luciousmc@icloud.com>
2020-02-06 21:20:18 -0800

    array-filter

--- exercises/array-filter/main.js
+++ exercises/array-filter/main.js
@@ -14,3 +14,31 @@ const names = [
   'Andy',
   'Tim D.'
 ];
+
+const evenNumbers = numbers.filter((number) => {
+  if (number % 2 === 0) {
+    return number;
+  }
+})
+console.log('evenNumbers: ', evenNumbers);
+
+const overFive = numbers.filter((number)=> {
+  if (number > 5) {
+    return number;
+  }
+})
+console.log('overFive: ', overFive);
+
+const startWithT = names.filter((name)=> {
+  if (name[0] === 'T') {
+    return name;
+  }
+})
+console.log('startWithT: ', startWithT);
+
+const hasD = names.filter(name=> {
+   if (name.includes('d') || name.includes('D')) {
+     return name;
+   }
+})
+console.log('hasD: ', hasD);
