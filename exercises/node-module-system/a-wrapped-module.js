548f6360600e0ce13023f28c8c3ac32a59e35a7c
Marlon <luciousmc@icloud.com>
2020-02-07 22:46:06 -0800

    node module system

--- exercises/node-module-system/a-wrapped-module.js
+++ exercises/node-module-system/a-wrapped-module.js
@@ -1,1 +1,33 @@
 /* eslint-disable no-console */
+// print the typeof exports to the terminal
+console.log('type of exports: ', typeof exports);
+
+// print the value of exports to the terminal
+console.log('value of exports: ', exports);
+
+// print the typeof require to the terminal
+console.log('type of require: ', typeof require);
+
+// print the value of require to the terminal
+console.log('value of require: ', require);
+
+// print the typeof module to the terminal
+console.log('type of module: ', typeof module);
+
+// print the value of module to the terminal
+console.log('value of module: ', module);
+
+// print the typeof __filename to the terminal
+console.log('type of __filename: ', typeof __filename);
+
+// print the value of __filename to the terminal
+console.log('value of __filename: ', __filename);
+
+// print the typeof __dirname to the terminal
+console.log('type of __dirname: ', __dirname);
+
+// print the value of __dirname to the terminal
+console.log('value of __dirname: ', __dirname);
+
+
+
