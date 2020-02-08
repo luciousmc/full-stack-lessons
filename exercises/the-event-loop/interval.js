f3abb7118abb24439691948de81279ff83d85d62
Marlon <luciousmc@icloud.com>
2020-02-07 22:00:30 -0800

    the event loop

--- exercises/the-event-loop/interval.js
+++ exercises/the-event-loop/interval.js
@@ -1,1 +1,11 @@
 /* eslint-disable no-console */
+let currentCount = 3;
+
+let countDown = setInterval(() => {
+  if (currentCount > 0) {
+    console.log(currentCount--);
+  } else {
+    console.log('Blast Off!');
+    clearInterval(countDown);
+  }
+}, 1000);
--- exercises/the-event-loop/timeout.js
+++ exercises/the-event-loop/timeout.js
@@ -1,1 +1,3 @@
 /* eslint-disable no-console */
+console.log('Hold up, wait a minute');
+let timer = setTimeout(console.log, 5000, 'Thank you for waiting');
