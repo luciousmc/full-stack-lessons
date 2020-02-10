a53941716a2d71891103975b817ff12a757d5bd0
Marlon <luciousmc@icloud.com>
2020-02-09 23:49:45 -0800

    operation modules created calculate function created addition completed

--- exercises/node-require/calculate.js
+++ exercises/node-require/calculate.js
@@ -1,1 +1,21 @@
 /* eslint-disable no-console */
+const add = require('./add');
+const subtract = require('./subtract');
+const multiply = require('./multiply');
+const divide = require('./divide');
+
+function calculate(num1, op, num2) {
+  switch(op) {
+    case '+':
+    case 'plus':
+      return num1 + num2;
+      break;
+    default:
+      return 'please use \'+\' symbol or plus';
+  }
+}
+const numInput1 = parseInt(process.argv[2]);
+const opInput = process.argv[3];
+const numInput2 = parseInt(process.argv[4]);
+const output = calculate(numInput1, opInput, numInput2);
+console.log('Answer: ', output);
