bab98fad63ec7ffbb10dfad111aee1e78a6472e8
Marlon <luciousmc@icloud.com>
2020-02-09 23:56:59 -0800

    node calc app completed

--- exercises/node-require/calculate.js
+++ exercises/node-require/calculate.js
@@ -10,8 +10,22 @@ function calculate(num1, op, num2) {
     case 'plus':
       return num1 + num2;
       break;
+    case '-':
+    case 'minus':
+      return num1 - num2;
+      break;
+    case 'x':
+    case 'X':
+    case 'times':
+      return num1 * num2;
+      break;
+    case '/':
+    case 'divide':
+    case 'over':
+      return num1 / num2;
+      break;
     default:
-      return 'please use \'+\' symbol or plus';
+      return 'Error: invalid operation';
   }
 }
 const numInput1 = parseInt(process.argv[2]);
