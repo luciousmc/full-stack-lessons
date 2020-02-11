344f58a5fabbeed647079f76808103601e8e9367
Marlon <luciousmc@icloud.com>
2020-02-11 00:03:21 -0800

    cp command completed

---
+++ exercises/node-fs-writefile/cp.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+
+const cpSource = process.argv[2];
+const cpDest = process.argv[3];
+
+fs.readFile(cpSource, 'utf8', (err, data)=> {
+  if (err) throw err;
+  fs.writeFile(cpDest, data, (err)=> {
+    if (err) throw err;
+    console.log('Copied!');
+  })
+})
