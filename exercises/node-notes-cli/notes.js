1c418d0b1bc9a4d6c014c3c17dc2c96aaf8eead0
Marlon <luciousmc@icloud.com>
2020-02-11 23:46:24 -0800

    create command completed

--- exercises/node-notes-cli/notes.js
+++ exercises/node-notes-cli/notes.js
@@ -4,7 +4,26 @@ const inputCmd = process.argv[2];
 
 switch (inputCmd) {
   case 'read':
-    for (const key in list.notes) {
-      console.log(`${key}: ${list.notes[key]}`)
-    }
+    readAll();
+    break;
+  case 'create':
+    const inputItem = process.argv[3];
+    createItem(inputItem);
+    readAll();
+    break;
+  case 'delete':
+    break;
+  case 'update':
+    break;
+  default:
+    console.log('Error: unrecognized command: ' + inputCmd);
+}
+function readAll () {
+  for (const key in list.notes) {
+    console.log(`${key}: ${list.notes[key]}`)
+  }
+}
+function createItem (item) {
+    list.notes[list.nextId.toString()] = item;
+    list.nextId++
 }
