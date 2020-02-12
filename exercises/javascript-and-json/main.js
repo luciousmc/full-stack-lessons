5a49ca2d2064f9a7ebe8344e8a78937ce7634070
Marlon <luciousmc@icloud.com>
2020-02-11 22:01:52 -0800

    basic skeleton setup books array created

---
+++ exercises/javascript-and-json/main.js
@@ -0,0 +1,18 @@
+const testArr = [
+  {
+    title: 'The Land Before Time',
+    author: 'Bobby Brown',
+    isbn: '100'
+  },
+  {
+    title: 'Monkey See, Monkey Do',
+    author: 'Ben Dover',
+    isbn: '300'
+  },
+  {
+    title: 'If You Can\'t Pull a Saw Out, Don\'t Push it in',
+    author: 'Bolson Colson',
+    isbn: '800'
+  }
+];
+
