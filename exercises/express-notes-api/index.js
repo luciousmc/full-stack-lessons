8924902e17f25928d9a18b659c0eccc0e8a24264
Marlon <luciousmc@icloud.com>
2020-02-18 01:03:34 -0800

    POST request handled and completed

--- exercises/express-notes-api/index.js
+++ exercises/express-notes-api/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const server = express();
 const grades = require('./data.json');
+const fs = require('fs');
 
 // User can retrieve all notes
 server.get('/api/notes', (req, res) => {
-  res.json(grades);
+  res.json(grades.notes);
 });
 
 // User can retrieve 1 note via its ID
@@ -21,6 +22,21 @@ server.get('/api/notes/:id', (req, res) => {
     return res.status(404).json({ error: `Cannot find id ${id}` });
   }
   res.json(grades.notes[id]);
+});
+
+// User can POST a new note 
+server.use('/api/notes', express.json(), (req, res) => {
+    if (!req.body.content) {
+    return res.status(400).json({ error: 'content property is required for POST' }); 
+  }
+  req.body.id = grades.nextId++;
+  grades.notes[req.body.id.toString()] = req.body;
+  fs.writeFile('./data.json', JSON.stringify(grades, null, 2), (err) => {
+    if (err) {
+      return res.status(500).json({ error: 'Sorry there was an unexpected error' });
+    }
+    res.status(201).json(req.body);
+  }) 
 });
 
 server.listen(3000, () => {
