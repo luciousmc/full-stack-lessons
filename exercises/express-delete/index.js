972bc564433597aff91e9d1ba5ddcb36c1acc8ce
Marlon <luciousmc@icloud.com>
2020-02-17 22:32:02 -0800

    express-delete

--- exercises/express-delete/index.js
+++ exercises/express-delete/index.js
@@ -20,6 +20,16 @@ server.get('/api/grades', (req, res, next) => {
   res.json(students);
 });
 
+server.delete('/api/grades/:id', (req, res, next) => {
+  const id = req.params.id;
+  for (let student = 0; student < students.length; student++) {
+    if (id == students[student].id) {
+      students.splice(student, 1);
+      return res.sendStatus(204);
+    } 
+  }
+  res.send('Student ID of \'' + id + '\' doesn\'t exist');
+});
 
 server.listen(3000, () => {
   console.log('Exprss is listening on port 3000');
