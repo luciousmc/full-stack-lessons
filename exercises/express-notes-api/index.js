bb46a670a43766fab6303cd0dbcaa57c02c8d8a7
Marlon <luciousmc@icloud.com>
2020-02-17 23:49:57 -0800

    adds error codes 400 and 404 for invalid requests

--- exercises/express-notes-api/index.js
+++ exercises/express-notes-api/index.js
@@ -2,8 +2,25 @@ const express = require('express');
 const server = express();
 const grades = require('./data.json');
 
-server.get('/api/grades', (req, res) => {
+// User can retrieve all notes
+server.get('/api/notes', (req, res) => {
   res.json(grades);
+});
+
+// User can retrieve 1 note via its ID
+server.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+
+  // ERROR if id is not a positive number
+  if (isNaN(id) || id < 1) {
+    return res.status(400).json({ error: 'ID must be a positive integer'});
+  }
+
+  // ERROR if id does not exist in note list
+  if (!grades.notes.hasOwnProperty(id)) {
+    return res.sendStatus(404);
+  }
+  res.json(grades.notes[id]);
 });
 
 server.listen(3000, () => {
