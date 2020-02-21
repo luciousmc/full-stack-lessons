65ecb39c520abb9968dbd7cd30086da3f9ca7118
Marlon <luciousmc@icloud.com>
2020-02-21 11:56:14 -0800

    react-app-drawer-component

--- exercises/react-app-drawer-component/src/app-drawer.jsx
+++ exercises/react-app-drawer-component/src/app-drawer.jsx
@@ -6,12 +6,23 @@ class AppDrawer extends React.Component {
     this.state = {
       isOpen: false
     }
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState( prevState => ({ isOpen: !prevState.isOpen }) )
   }
   render() {
+    const overlayToggle = this.state.isOpen ? 'show' : 'hidden';
+    const menuToggle = this.state.isOpen ? 'open' : 'closed';
     return(
       <React.Fragment>
-        <div className="menu-overlay"></div>
-        <menu>
+        <div className="icon">
+          <span className="bars" onClick={ this.handleClick }>
+            <i className="fas fa-bars"></i>
+          </span>
+        </div>
+        <div className={ `menu-overlay ${ overlayToggle }`} onClick={ this.handleClick }></div>
+        <menu className={ menuToggle }>
           <h1 className="menu-title">Menu</h1>
           <nav className="nav">
             <ul className="nav-bar">
