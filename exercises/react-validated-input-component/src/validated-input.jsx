4d05a6918a719ac174070122148ffa1428b7e2e4
Marlon <luciousmc@icloud.com>
2020-02-21 02:02:28 -0800

    adds css grid

--- exercises/react-validated-input-component/src/validated-input.jsx
+++ exercises/react-validated-input-component/src/validated-input.jsx
@@ -4,7 +4,9 @@ class ValidatedInput extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: ''
+      value: '',
+      errMsg: '',
+      isValid: false
     }
     this.handleChange = this.handleChange.bind(this);
   }
@@ -13,14 +15,40 @@ class ValidatedInput extends React.Component {
   }
   handleSubmit(e) {
     e.preventDefault();
+    const passFail = this.checkValidInput(this.state.value);
+    if (passFail) {
+      this.setState({ isValid: true });
+    }
+    this.setState({ isValid: false });
+  }
+  checkValidInput(input) {
+    if (!input) {
+      this.setState({ errMsg: 'Password is required' });
+      return false;
+    } else if (input.length < 8) {
+      this.setState({ errMsg: 'Password must be at least 8 characters' });
+      return false;
+    } else {
+      return true;
+    }
   }
   render() {
-    <form onSubmit={ this.handleSubmit }>
-      <label>
-        Password: 
-      </label>
-      <input type="text" name="password" id="password" onChange={ this.handleChange } />
-      <input type="submit" name="submit" />
-    </form>
+    const iconClass = this.state.isValid ? 'fa-check' : 'fa-times';
+    return(
+      <div className="container">
+        <form onSubmit={ this.handleSubmit }>
+          <label>
+            Password: 
+          </label>
+          <input type="text" name="password" id="password" onChange={ this.handleChange } />
+          <input type="submit" name="submit" />
+        </form>
+        <div className="icon-container">
+          <i className={ `fas ${iconClass}` }></i>
+        </div>
+      </div>
+    )
   }
 }
+
+export default ValidatedInput;
