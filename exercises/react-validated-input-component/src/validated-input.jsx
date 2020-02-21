3b26c400b6c4f0cd7f4e7b372763903761cb47ac
Marlon <luciousmc@icloud.com>
2020-02-21 10:37:33 -0800

    validated-input-component

--- exercises/react-validated-input-component/src/validated-input.jsx
+++ exercises/react-validated-input-component/src/validated-input.jsx
@@ -9,28 +9,60 @@ class ValidatedInput extends React.Component {
       isValid: false
     }
     this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
   handleChange(e) {
     this.setState({ value: e.target.value });
   }
   handleSubmit(e) {
+    // Prevent form from sending request
     e.preventDefault();
-    const passFail = this.checkValidInput(this.state.value);
-    if (passFail) {
+
+    if (this.validateInput(this.state.value)) {
       this.setState({ isValid: true });
     }
-    this.setState({ isValid: false });
   }
-  checkValidInput(input) {
+  validateInput(input) {
+    // ERROR if there is no input
     if (!input) {
       this.setState({ errMsg: 'Password is required' });
-      return false;
+
+    // ERROR if user enters less than 8 characters
     } else if (input.length < 8) {
       this.setState({ errMsg: 'Password must be at least 8 characters' });
-      return false;
+
+    // Check for capital letters
+    // ERROR if password has no capital letters
+    } else if (!this.checkCaps(input)) {
+      this.setState({ errMsg: 'Password must have at least 1 capital letter' });
+
+    // Check for numbers
+    // ERROR if password has no numbers
+    } else if (!this.checkNum(input)) {
+      this.setState({ errMsg: 'Password must have at least 1 number' });
+
+    // Check for special characters
+    // ERROR if none are found
+    } else if (!this.checkSpecChar(input)) {
+      this.setState({ errMsg: 'Password must have at least 1 special character' });
+
+    // Return true and clear any error messages if the input passes all tests
     } else {
+      this.setState({ errMsg: '' });
       return true;
     }
+  }
+  checkCaps(input) {
+    const regex = /[A-Z]/;
+    return regex.test(input);
+  }
+  checkNum(input) {
+    const regex = /[0-9]/;
+    return regex.test(input);
+  }
+  checkSpecChar(input) {
+    const regex = /[-!@#$%^&*(){};:'"`~,<\.>/\\?|_]/;
+    return regex.test(input);
   }
   render() {
     const iconClass = this.state.isValid ? 'fa-check' : 'fa-times';
@@ -40,7 +72,8 @@ class ValidatedInput extends React.Component {
           <label>
             Password: 
           </label>
-          <input type="text" name="password" id="password" onChange={ this.handleChange } />
+          <input type="password" name="password" id="password" onChange={ this.handleChange } />
+          <div className="error-msg">{ this.state.errMsg }</div>
           <input type="submit" name="submit" />
         </form>
         <div className="icon-container">
