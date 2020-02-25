bb459e42a5cba345debf2add16cf2211c6609ad2
Marlon <luciousmc@icloud.com>
2020-02-25 12:59:46 -0800

    fetch

--- exercises/fetch/main.js
+++ exercises/fetch/main.js
@@ -1,1 +1,12 @@
 /* eslint-disable no-console */
+
+fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => {
+    return response.json();
+  })
+  .then(json => {
+    console.log('the response object is ', json);
+  })
+  .catch(error => {
+    console.log(error);
+  })
