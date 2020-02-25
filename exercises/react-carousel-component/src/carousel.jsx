51af6318d516bb75d3bb9bf131ffc65fab79319e
Marlon <luciousmc@icloud.com>
2020-02-25 01:04:47 -0800

    fits images in slider container

--- exercises/react-carousel-component/src/carousel.jsx
+++ exercises/react-carousel-component/src/carousel.jsx
@@ -16,15 +16,37 @@ class Slider extends React.Component {
     this.state = {
       currImg: 0
     }
+    this.timerID = null;
     this.imgCount = this.props.images.length;
     this.handlePrevClick = this.handlePrevClick.bind(this);
     this.handleNextClick = this.handleNextClick.bind(this);
   }
+  componentDidMount() {
+    this.autoScroll();
+  }
+  autoScroll() {
+    this.timerID = setInterval(()=> {
+      if (this.state.currImg === this.imgCount - 1) {
+        this.setState({ currImg: 0 });
+      } else {
+        this.setState(state=> ({ currImg: state.currImg + 1 }))
+      }
+    }, 3000)
+  }
+  delayScroll() {
+    clearInterval(this.timerID);
+
+    setTimeout(()=> {
+      this.autoScroll();
+    }, 6000);
+  }
   handlePrevClick() {
+    this.delayScroll();
     if (this.state.currImg === 0) return;
     this.setState(state=> ({ currImg: state.currImg - 1 }));
   }
   handleNextClick() {
+    this.delayScroll();
     if (this.state.currImg === this.imgCount - 1) return;
     this.setState(state=> ({ currImg: state.currImg + 1 }));
   }
@@ -35,9 +57,9 @@ class Slider extends React.Component {
           <div className="btn-previous-container" onClick={ this.handlePrevClick }>
             <div className="btn-previous"></div>
           </div>
-          <figure className="image">
-            <ImageList images={ this.props.images } currImg={ this.state.currImg } />
-          </figure>
+          <div className="img-container">
+           <ImageList images={ this.props.images } currImg={ this.state.currImg } />
+          </div>
           <div className="btn-next-container" onClick={ this.handleNextClick }>
             <div className="btn-next"></div>
           </div>
@@ -52,11 +74,15 @@ class Slider extends React.Component {
 
 class ImageList extends React.Component {
   render() {
-    return this.props.images.map((image, index)=> {
-      return(
-        <img src={ image } alt={ `Slider Image ${index + 1}` } key={ index } />
-      )
-    })
+    const currImg = this.props.currImg;
+    let img = this.props.images.filter((image, index) => {
+      if (currImg === index) return true;
+    });
+    return(
+      <figure className="image">
+        <img src={ img } alt={ `Slider Image` } />
+      </figure>
+    )
   }
 }
 
