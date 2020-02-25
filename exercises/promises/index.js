2ca39d641590ea1f88bb2e08b9031e5e8ab10aa3
Marlon <luciousmc@icloud.com>
2020-02-25 12:17:11 -0800

    promises

--- exercises/promises/index.js
+++ exercises/promises/index.js
@@ -1,2 +1,10 @@
 /* eslint-disable no-unused-vars, no-console */
 const takeAChance = require('./take-a-chance');
+
+const promise = takeAChance('Marlon');
+
+promise.then((value)=> {
+  console.log(value);
+}, (err)=> {
+  console.error(err);
+});
