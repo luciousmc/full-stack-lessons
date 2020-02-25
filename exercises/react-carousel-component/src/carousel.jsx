f5bba98c33269027cc4825665811b3c3234599e2
Marlon <luciousmc@icloud.com>
2020-02-24 19:50:08 -0800

    refactored code by separating to more components

--- exercises/react-carousel-component/src/carousel.jsx
+++ exercises/react-carousel-component/src/carousel.jsx
@@ -1,34 +1,72 @@
 import React from 'react';
 
-class Carousel extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      imgList: this.props.images,
-      currentImg: 0
-    }
-    
-  }
+export default class Carousel extends React.Component {
   render() {
- 
     return(
-      <div className="carousel">
-        <div className="arrow-previous"></div>
-
-        <figure className="img-container">
-         <img src="images/pikachu.png" alt="PIkacu"/>
-         <img src="images/squirtle.png" alt=""/>
-        </figure>
-        
-        <div className="dots-container">
-          <div className="dot-nav active"></div>
-          <div className="dot-nav"></div>
-          <div className="dot-nav"></div>
-          <div className="dot-nav"></div>
+      <section className="carousel">
+        <div className="slider-container">
+          <Slider images={ this.props.images } />
         </div>
-      </div>
-    );
+      </section>
+    )
   }
 }
 
-export default Carousel;
+class Slider extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currImg: 0
+    }
+    this.imgCount = this.props.images.length;
+    this.handlePrevClick = this.handlePrevClick.bind(this);
+    this.handleNextClick = this.handleNextClick.bind(this);
+  }
+  handlePrevClick() {
+    if (this.state.currImg === 0) return;
+    this.setState(state=> ({ currImg: state.currImg - 1 }));
+  }
+  handleNextClick() {
+    if (this.state.currImg === this.imgCount - 1) return;
+    this.setState(state=> ({ currImg: state.currImg + 1 }));
+  }
+  render() {
+    return(
+      <>
+        <div>
+          <div className="btn-previous" onClick={ this.handlePrevClick }></div>
+          <figure className="image">
+            {/* <img src={ this.props.images[this.state.currImg] } alt=""/> */}
+            <ImageList images={ this.props.images } currImg={ this.state.currImg } />
+          </figure>
+          <div className="btn-next" onClick={ this.handleNextClick }></div>
+        </div>
+        <div className="dots">
+          <Dots images={ this.props.images } currImg={ this.state.currImg } />
+        </div>
+      </>
+    )
+  }
+}
+
+class ImageList extends React.Component {
+  render() {
+    return this.props.images.map((image, index)=> {
+      return(
+        <img src={ image } alt={ `Slider Image ${index + 1}` } key={ index } />
+      )
+    })
+  }
+}
+
+class Dots extends React.Component {
+  render() {
+    const currImg = this.props.currImg;
+
+    return this.props.images.map((image, index)=> {
+      return(
+        <div className={ `dot ${ currImg === index ? 'active' : ' '}` } key={ index }></div>
+      )
+    })
+  }
+}
